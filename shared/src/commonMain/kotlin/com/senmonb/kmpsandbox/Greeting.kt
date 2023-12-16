d6858acf5773171dac8f99654ead9c8cdb0b1c70
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-12-16 11:23:00 +0900

    change greeting

--- shared/src/commonMain/kotlin/com/senmonb/kmpsandbox/Greeting.kt
+++ shared/src/commonMain/kotlin/com/senmonb/kmpsandbox/Greeting.kt
@@ -1,9 +1,13 @@
 package com.senmonb.kmpsandbox
 
+import kotlin.random.Random
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        val firstWord = if (Random.nextBoolean()) "HI!" else "Hello!"
+
+        return "$firstWord Guess what this is! > ${platform.name.reversed()}!"
     }
 }
